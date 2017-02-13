03914f312b55f4d9602d2c5752cd70a39d5847cd
wesniem <wesniem@yahoo.com>
2017-02-12 19:06:07 -0500

    Settings updated

--- app/src/main/java/nyc/c4q/wesniemarcelin/finalexampractical/SettingsActivity.java
+++ app/src/main/java/nyc/c4q/wesniemarcelin/finalexampractical/SettingsActivity.java
@@ -3,14 +3,23 @@ package nyc.c4q.wesniemarcelin.finalexampractical;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import nyc.c4q.wesniemarcelin.finalexampractical.recyclerView.SettingsAdapter;
 
 /**
  * Created by wesniemarcelin on 2/12/17.
  */
 public class SettingsActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_layout);
+        recyclerView = (RecyclerView) findViewById(R.id.number_rv);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new SettingsAdapter());
     }
 }
